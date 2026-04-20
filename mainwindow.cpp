07591c60bfe65ccf961bd524eaf2ba890221abad
jung_wui <sonbal7762@gmail.com>
2026-04-20 20:51:36 +0900

    feat: 버그 수정 및 edit 오류 추가

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,162 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+#include "schedulemanager.h"
+#include "scheduleeditordialog.h"
+
+void applyStyleSheet(QWidget* widget, const QString& path) {
+    QFile file(path);
+    if (file.open(QFile::ReadOnly)) {
+        QString styleSheet = QString::fromUtf8(file.readAll());
+        widget->setStyleSheet(styleSheet);
+    }
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+
+    applyStyleSheet(this, ":/style/style.qss");
+
+    ui->cwCalender->setVerticalHeaderFormat(QCalendarWidget::NoVerticalHeader);
+
+    QTextCharFormat saturdayFormat;
+    saturdayFormat.setForeground(QColor("#007bff"));
+    saturdayFormat.setFontWeight(QFont::Bold);
+    ui->cwCalender->setWeekdayTextFormat(Qt::Saturday, saturdayFormat);
+
+    QTextCharFormat sundayFormat;
+    sundayFormat.setForeground(QColor("#dc3545"));
+    sundayFormat.setFontWeight(QFont::Bold);
+    ui->cwCalender->setWeekdayTextFormat(Qt::Sunday, sundayFormat);
+
+    scheduleManager = new ScheduleManager(this);
+    connect(scheduleManager, &ScheduleManager::schedulesChanged, this, &MainWindow::updateTable);
+    connect(ui->leSearch, &QLineEdit::textChanged, this, &MainWindow::updateTable);
+
+    scheduleManager->loadSchedules();
+
+    ui->twScheduleList->verticalHeader()->setVisible(false);
+    ui->twScheduleList->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Stretch);
+    ui->twScheduleList->horizontalHeader()->setSectionResizeMode(1, QHeaderView::ResizeToContents);
+    ui->twScheduleList->horizontalHeader()->setSectionResizeMode(2, QHeaderView::ResizeToContents);
+
+    selectedDate = QDate::currentDate();
+    ui->lblSelectedDate->setText(selectedDate.toString("yyyy-MM-dd"));
+    updateTable();
+}
+
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::on_cwCalender_selectionChanged() {
+    selectedDate = ui->cwCalender->selectedDate();
+    ui->lblSelectedDate->setText(selectedDate.toString("yyyy-MM-dd"));
+    updateTable();
+}
+
+void MainWindow::updateTable() {
+    ui->twScheduleList->setRowCount(0);
+
+    QString searchText = ui->leSearch->text();
+
+    // 화면 데이터 결정
+    if (searchText.isEmpty()) {
+        currentViewList = scheduleManager->getSchedulesForDate(selectedDate);
+    } else {
+        currentViewList = scheduleManager->getSchedulesByContainText(searchText);
+    }
+
+    // 테이블에 뿌리기
+    for (int i = 0; i < currentViewList.size(); i++) {
+        const Schedule &schedule = currentViewList[i];
+
+        ui->twScheduleList->insertRow(i);
+        ui->twScheduleList->setItem(i, 0, new QTableWidgetItem(schedule.getTitle()));
+        ui->twScheduleList->setItem(i, 1, new QTableWidgetItem(schedule.getStartTime().toString("yyyy-MM-dd")));
+        ui->twScheduleList->setItem(i, 2, new QTableWidgetItem(schedule.getEndTime().toString("yyyy-MM-dd")));
+    }
+}
+
+void MainWindow::on_btnAdd_clicked() {
+    ScheduleEditorDialog dlg(this);
+    // 날짜 전달
+    dlg.setDate(selectedDate);
+
+    connect(&dlg, &ScheduleEditorDialog::scheduleSaved,
+            this, [this](const Schedule &s){
+                scheduleManager->addSchedule(s);
+                scheduleManager->saveSchedules();
+
+                qDebug() << "시작 날짜:" << s.getStartTime().toString(Qt::ISODate);
+                qDebug() << "끝 날짜:" << s.getEndTime().toString(Qt::ISODate);
+                qDebug() << "저장 완료";
+            });
+
+    dlg.exec();
+}
+
+void MainWindow::on_btnEdit_clicked() {
+    int row = ui->twScheduleList->currentRow();
+    if (row < 0) {
+        QMessageBox::warning(this, "오류", "수정할 항목을 선택해주세요.");
+        return;
+    }
+
+    Schedule selected = currentViewList[row];
+
+    ScheduleEditorDialog dlg(this);
+    dlg.setSchedule(selected);
+
+    connect(&dlg, &ScheduleEditorDialog::scheduleSaved,
+            this, [this, selected](const Schedule &s){
+
+                // 기존 데이터 찾아서 수정
+                QList<Schedule> all = scheduleManager->getSchedules();
+
+                for (int i = 0; i < all.size(); i++) {
+                    if (all[i].getStartTime() == selected.getStartTime() &&
+                        all[i].getTitle() == selected.getTitle()) {
+
+                        scheduleManager->updateSchedule(i, s);
+                        break;
+                    }
+                }
+
+                scheduleManager->saveSchedules();
+            });
+
+    dlg.exec();
+}
+
+void MainWindow::on_btnRemove_clicked() {
+    int row = ui->twScheduleList->currentRow();
+    if (row < 0) {
+        QMessageBox::warning(this, "오류", "삭제할 항목을 선택해주세요.");
+        return;
+    }
+
+    if (QMessageBox::question(this, "삭제", "정말 삭제하시겠습니까?")
+        != QMessageBox::Yes) {
+        return;
+    }
+
+    // 화면 기준 데이터 사용
+    Schedule selected = currentViewList[row];
+
+    // 전체 데이터에서 찾아서 삭제
+    QList<Schedule> all = scheduleManager->getSchedules();
+
+    for (int i = 0; i < all.size(); i++) {
+        if (all[i].getStartTime() == selected.getStartTime() &&
+            all[i].getTitle() == selected.getTitle()) {
+
+            scheduleManager->removeSchedule(i);
+            break;
+        }
+    }
+    // 저장
+    scheduleManager->saveSchedules();
 }
