544c892f48f1eb98e798c18ea16e088ef69e174a
dldyou <chan12_yun@naver.com>
2026-04-20 22:12:53 +0900

    Revert "feat: 버그 수정 및 edit 오류 추가"
    This reverts commit 07591c60bfe65ccf961bd524eaf2ba890221abad.

--- mainwindow.h
+++ mainwindow.h
@@ -2,41 +2,22 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QTableWidget>
-#include <QTableWidgetItem>
-#include <QDate>
-#include <QList>
 
-#include "schedulemanager.h"
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
 
-private slots:
-    void on_cwCalender_selectionChanged();
-
-    void on_btnAdd_clicked();
-
-    void on_btnEdit_clicked();
-
-    void on_btnRemove_clicked();
-
 private:
     Ui::MainWindow *ui;
-    QDate selectedDate;
-    ScheduleManager *scheduleManager;
-
-    QList<Schedule> currentViewList; // 추가
-
-    void updateTable();
 };
 #endif // MAINWINDOW_H
